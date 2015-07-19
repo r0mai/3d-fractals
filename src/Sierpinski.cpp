8ad43925a2368ff05bdd859f7d2acd9f39e6cbf2
Andras Kucsma <andras.kucsma@prezi.com>
2015-07-19 18:39:46 +0200

    Draw a triangle (not visible yet)

--- src/Sierpinski.cpp
+++ src/Sierpinski.cpp
@@ -43,10 +43,51 @@ Sierpinski::~Sierpinski() {
 
 void Sierpinski::run() {
     init();
+
+    const GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f,
+    };
+
+    // This will identify our vertex buffer
+    GLuint vertexbuffer;
+
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers(1, &vertexbuffer);
+
+    // The following commands will talk about our 'vertexbuffer' buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+
+    // Give our vertices to OpenGL.
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        sizeof(g_vertex_buffer_data),
+        g_vertex_buffer_data,
+        GL_STATIC_DRAW);
+
     do {
+        // 1st attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+           0,                  // attribute 0.
+           3,                  // size
+           GL_FLOAT,           // type
+           GL_FALSE,           // normalized?
+           0,                  // stride
+           (void*)0            // array buffer offset
+        );
+
+        // Draw the triangle !
+        // Starting from vertex 0; 3 vertices total -> 1 triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glDisableVertexAttribArray(0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
-    } while (glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+    } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
         glfwWindowShouldClose(window) == 0 );
 }
 
